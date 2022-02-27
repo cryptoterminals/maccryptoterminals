5e49288a2014d434096cac970d454891023e5052
cryptoterminals <80123683+cryptoterminals@users.noreply.github.com>
2022-02-27 12:51:03 -0800

    fix indentation (punks price update)

--- terminal_micropython.py
+++ terminal_micropython.py
@@ -29,46 +29,46 @@ uos.dupterm(None, 1)
 rtc = RTC()
 
 def sendserial(command):
-	uart.write(str(command))
-	uart.write(str("*"))
+    uart.write(str(command))
+    uart.write(str("*"))
 
 def callcg():
- 	while True:
-		try:
- 			response=urequests.get(coingeckorequest)
-			response = json.loads(response.content.decode('utf-8'))
-			return response
-		except:
-			sendserial("CGCALLFAIL")
-			utime.sleep(10)
+    while True:
+        try:
+            response=urequests.get(coingeckorequest)
+            response = json.loads(response.content.decode('utf-8'))
+            return response
+        except:
+            sendserial("CGCALLFAIL")
+            utime.sleep(10)
 
 def check_connection():
-	while (wlan.isconnected() == False):
-		sendserial("NOCO")
-		#utime.sleep(5)
-		input = uart.readline()
-		if input != None:
-			decoded_input = input.decode("utf-8")
-			#sendserial(decoded_input) # For debug only
-			if (decoded_input[0:3] == "AP:"):
-				apstop = decoded_input.find('\r')
-				if (apstop > 1):
-					APNAME = decoded_input[3:apstop]
-					if (decoded_input[apstop+1:apstop+6] == "PASS:"):
-						pwstop = decoded_input.find('\r', apstop+6)
-						if (pwstop > 1):
-							PASSWORD = decoded_input[apstop+6:pwstop]
-							wlan.connect(APNAME, PASSWORD)
-			utime.sleep(10)
-		else:
-			pass
-	else:
-		try:
-			ntptime.settime()
-			sendserial(".")
-			pass
-		except:
-			sendserial("NTPFAIL")
+    while (wlan.isconnected() == False):
+        sendserial("NOCO")
+        #utime.sleep(5)
+        input = uart.readline()
+        if input != None:
+            decoded_input = input.decode("utf-8")
+            #sendserial(decoded_input) # For debug only
+            if (decoded_input[0:3] == "AP:"):
+                apstop = decoded_input.find('\r')
+                if (apstop > 1):
+                    APNAME = decoded_input[3:apstop]
+                    if (decoded_input[apstop+1:apstop+6] == "PASS:"):
+                        pwstop = decoded_input.find('\r', apstop+6)
+                        if (pwstop > 1):
+                            PASSWORD = decoded_input[apstop+6:pwstop]
+                            wlan.connect(APNAME, PASSWORD)
+            utime.sleep(10)
+        else:
+            pass
+    else:
+        try:
+            ntptime.settime()
+            sendserial(".")
+            pass
+        except:
+            sendserial("NTPFAIL")
 
 realoffset = 0
 calltimer = 60
@@ -83,123 +83,123 @@ usdt_last_call_timestamp = 0
 dai_last_call_timestamp = 0
 
 def printprice(prefix, value):
-	if ((prefix == "USDC") or (prefix == "USDT") or (prefix == "DAIP")):
-		formatted_price = prefix + "$" + '{:,.2f}'.format(value)
-		#formatted_price = prefix + "$" + str(value)    ### was commented before LEAVE!
-	elif (type(value) is float):
-		value = int(value)
-		#formatted_price = prefix + "$" + '{:,.0f}'.format(value)
-		formatted_price = prefix + "$" + '{:,}'.format(value)
-	elif (type(value) is int):
-		formatted_price = prefix + "$" + "{:,}".format(value)
-	sendserial(formatted_price)
+    if ((prefix == "USDC") or (prefix == "USDT") or (prefix == "DAIP")):
+        formatted_price = prefix + "$" + '{:,.2f}'.format(value)
+        #formatted_price = prefix + "$" + str(value)    ### was commented before LEAVE!
+    elif (type(value) is float):
+        value = int(value)
+        #formatted_price = prefix + "$" + '{:,.0f}'.format(value)
+        formatted_price = prefix + "$" + '{:,}'.format(value)
+    elif (type(value) is int):
+        formatted_price = prefix + "$" + "{:,}".format(value)
+    sendserial(formatted_price)
 
 def get_utc_offset(input):
-	if len(input) == 5:
-		value = int(input[4])
-		if input[3] == '-':
-			value = value * -1
-		return value
-	elif len(input) == 6:
-		value = int(input[4:6])
-		if input[3] == '-':
-			value = value * -1
-		return value
-	else:
-		return 0
+    if len(input) == 5:
+        value = int(input[4])
+        if input[3] == '-':
+            value = value * -1
+        return value
+    elif len(input) == 6:
+        value = int(input[4:6])
+        if input[3] == '-':
+            value = value * -1
+        return value
+    else:
+        return 0
 
-def update(btc_last_call_timestamp, eth_last_call_timestamp, usdc_last_call_timestamp, usdt_last_call_timestamp, dai_last_call_timestamp, realoffset):	
-	response = callcg()
-	if response['bitcoin']['last_updated_at']+realoffset == btc_last_call_timestamp:
-		pass
-	else:
-		printprice("BTCP", response['bitcoin']['usd'])
-		formattedtime = utime.localtime(response['bitcoin']['last_updated_at']+realoffset)
-		formattedtime = str(formattedtime[3]) + ":" + str("{:02d}".format(formattedtime[4]))
-		sendserial("BTCT" + str(formattedtime))
-		if (response['bitcoin']['usd_24h_change'] > 0):
-			sendserial("BTCD" + "UP  " + str("{:.2f}".format(response['bitcoin']['usd_24h_change'])) + "%")
-		elif (response['bitcoin']['usd_24h_change'] < 0):
-			sendserial("BTCD" + "down  " + str("{:.2f}".format(response['bitcoin']['usd_24h_change']*-1)) + "%")
-	if response['ethereum']['last_updated_at']+realoffset == eth_last_call_timestamp:
-		pass
-	else:
-		printprice("ETHP", response['ethereum']['usd'])
-		formattedtime = utime.localtime(response['ethereum']['last_updated_at']+realoffset)
-		formattedtime = str(formattedtime[3]) + ":" + str("{:02d}".format(formattedtime[4]))
-		sendserial("ETHT" + str(formattedtime))
-		if (response['ethereum']['usd_24h_change'] > 0):
-			sendserial("ETHD" + "UP  " + str("{:.2f}".format(response['ethereum']['usd_24h_change'])) + "%")
-		elif (response['ethereum']['usd_24h_change'] < 0):
-			sendserial("ETHD" + "down  " + str("{:.2f}".format(response['ethereum']['usd_24h_change']*-1)) + "%")
-	if response['usd-coin']['last_updated_at']+realoffset == usdc_last_call_timestamp:
-		pass
-	else:
-		printprice("USDC", response['usd-coin']['usd'])
-	if response['tether']['last_updated_at']+realoffset == usdt_last_call_timestamp:
-		pass
-	else:
-		printprice("USDT", response['tether']['usd'])
-	if response['dai']['last_updated_at']+realoffset == dai_last_call_timestamp:
-		pass
-	else:
-		printprice("DAIP", response['dai']['usd'])
+def update(btc_last_call_timestamp, eth_last_call_timestamp, usdc_last_call_timestamp, usdt_last_call_timestamp, dai_last_call_timestamp, realoffset):
+    response = callcg()
+    if response['bitcoin']['last_updated_at']+realoffset == btc_last_call_timestamp:
+        pass
+    else:
+        printprice("BTCP", response['bitcoin']['usd'])
+        formattedtime = utime.localtime(response['bitcoin']['last_updated_at']+realoffset)
+        formattedtime = str(formattedtime[3]) + ":" + str("{:02d}".format(formattedtime[4]))
+        sendserial("BTCT" + str(formattedtime))
+        if (response['bitcoin']['usd_24h_change'] > 0):
+            sendserial("BTCD" + "UP  " + str("{:.2f}".format(response['bitcoin']['usd_24h_change'])) + "%")
+        elif (response['bitcoin']['usd_24h_change'] < 0):
+            sendserial("BTCD" + "down  " + str("{:.2f}".format(response['bitcoin']['usd_24h_change']*-1)) + "%")
+    if response['ethereum']['last_updated_at']+realoffset == eth_last_call_timestamp:
+        pass
+    else:
+        printprice("ETHP", response['ethereum']['usd'])
+        formattedtime = utime.localtime(response['ethereum']['last_updated_at']+realoffset)
+        formattedtime = str(formattedtime[3]) + ":" + str("{:02d}".format(formattedtime[4]))
+        sendserial("ETHT" + str(formattedtime))
+        if (response['ethereum']['usd_24h_change'] > 0):
+            sendserial("ETHD" + "UP  " + str("{:.2f}".format(response['ethereum']['usd_24h_change'])) + "%")
+        elif (response['ethereum']['usd_24h_change'] < 0):
+            sendserial("ETHD" + "down  " + str("{:.2f}".format(response['ethereum']['usd_24h_change']*-1)) + "%")
+    if response['usd-coin']['last_updated_at']+realoffset == usdc_last_call_timestamp:
+        pass
+    else:
+        printprice("USDC", response['usd-coin']['usd'])
+    if response['tether']['last_updated_at']+realoffset == usdt_last_call_timestamp:
+        pass
+    else:
+        printprice("USDT", response['tether']['usd'])
+    if response['dai']['last_updated_at']+realoffset == dai_last_call_timestamp:
+        pass
+    else:
+        printprice("DAIP", response['dai']['usd'])
 
-	usdc_last_call_timestamp = response['usd-coin']['last_updated_at']+realoffset
-	usdt_last_call_timestamp = response['tether']['last_updated_at']+realoffset
-	dai_last_call_timestamp = response['dai']['last_updated_at']+realoffset
-	eth_last_call_timestamp = response['ethereum']['last_updated_at']+realoffset
-	btc_last_call_timestamp = response['bitcoin']['last_updated_at']+realoffset
-	return btc_last_call_timestamp, eth_last_call_timestamp, usdc_last_call_timestamp, usdt_last_call_timestamp, dai_last_call_timestamp
+    usdc_last_call_timestamp = response['usd-coin']['last_updated_at']+realoffset
+    usdt_last_call_timestamp = response['tether']['last_updated_at']+realoffset
+    dai_last_call_timestamp = response['dai']['last_updated_at']+realoffset
+    eth_last_call_timestamp = response['ethereum']['last_updated_at']+realoffset
+    btc_last_call_timestamp = response['bitcoin']['last_updated_at']+realoffset
+    return btc_last_call_timestamp, eth_last_call_timestamp, usdc_last_call_timestamp, usdt_last_call_timestamp, dai_last_call_timestamp
 
 def punksupdate(punkslast):
-	if ((punkslast == 0) or ((time() - punkslast) > 1800)):
-		try:
-			response = urequests.get(punksrequest)
-			punksfloorpos = response.text.find("punksfloor:")
-			punksfloorpos2 = response.text.find("ETH")
-			punksfloor = response.text[punksfloorpos+11:punksfloorpos2+3]
-			sendserial("PNKF" + punksfloor)
-			punkssalepos = response.text.find("punkslast:")
-			punkssalepos2 = response.text[punkssalepos:].find("ETH")
-			punkssale = response.text[punkssalepos+10:punkssalepos+punkssalepos2+3]
-			sendserial("PNKL" + punkssale)
-			punkslast = time()
-			return punkslast
-		except:
-			sendserial("PNKF" + "NA")
-			return punkslast
-	else:
-		return punkslast		
-		
+    if ((punkslast == 0) or ((time() - punkslast) > 1800)):
+        try:
+            response = urequests.get(punksrequest)
+            punksfloorpos = response.text.find("punksfloor:")
+            punksfloorpos2 = response.text.find("ETH")
+            punksfloor = response.text[punksfloorpos+11:punksfloorpos2+3]
+            sendserial("PNKF" + punksfloor)
+            punkssalepos = response.text.find("punkslast:")
+            punkssalepos2 = response.text[punkssalepos:].find("ETH")
+            punkssale = response.text[punkssalepos+10:punkssalepos+punkssalepos2+3]
+            sendserial("PNKL" + punkssale)
+            punkslast = time()
+            return punkslast
+        except:
+            sendserial("PNKF" + "NA")
+            return punkslast
+    else:
+        return punkslast
+
 
 while True:
-	check_connection()
-	if realoffset == 0:
-		sendserial("REQTZ")
-	btc_last_call_timestamp, eth_last_call_timestamp, usdc_last_call_timestamp, usdt_last_call_timestamp, dai_last_call_timestamp = update(btc_last_call_timestamp, eth_last_call_timestamp, usdc_last_call_timestamp, usdt_last_call_timestamp, dai_last_call_timestamp, realoffset)
-	punkslast = punksupdate(punkslast)
-	calltimer = 5
-	while calltimer > 0:
-		input = uart.read(500)
-		if (input != None):
-			decoded_input = input.decode("utf-8")
-			if (decoded_input == "UPDA"):
-				btc_last_call_timestamp, eth_last_call_timestamp, usdc_last_call_timestamp, usdt_last_call_timestamp = update(btc_last_call_timestamp, eth_last_call_timestamp, spi_last_call_timestamp, realoffset)
-			elif (decoded_input == "WIFIGO"):
-				sendserial("OKNEWWIFI...")
-				PASSWORD = '.'
-				AP = '.'
-				wlan.active(False)
-				wlan.active(True)
-				wlan.connect(APNAME, PASSWORD)
-			elif (decoded_input[0:3] == "UTC"):
-				timeoffset = get_utc_offset(decoded_input)
-				realoffset = timeoffset * 3600
-				#sendserial(str(timeoffset))
-			else:
-				sendserial(str(input))
-				pass
-				#uart.write(str(input))
-		utime.sleep(7)
-		calltimer -= 1
+    check_connection()
+    if realoffset == 0:
+        sendserial("REQTZ")
+    btc_last_call_timestamp, eth_last_call_timestamp, usdc_last_call_timestamp, usdt_last_call_timestamp, dai_last_call_timestamp = update(btc_last_call_timestamp, eth_last_call_timestamp, usdc_last_call_timestamp, usdt_last_call_timestamp, dai_last_call_timestamp, realoffset)
+    punkslast = punksupdate(punkslast)
+    calltimer = 5
+    while calltimer > 0:
+        input = uart.read(500)
+        if (input != None):
+            decoded_input = input.decode("utf-8")
+            if (decoded_input == "UPDA"):
+                btc_last_call_timestamp, eth_last_call_timestamp, usdc_last_call_timestamp, usdt_last_call_timestamp = update(btc_last_call_timestamp, eth_last_call_timestamp, spi_last_call_timestamp, realoffset)
+            elif (decoded_input == "WIFIGO"):
+                sendserial("OKNEWWIFI...")
+                PASSWORD = '.'
+                AP = '.'
+                wlan.active(False)
+                wlan.active(True)
+                wlan.connect(APNAME, PASSWORD)
+            elif (decoded_input[0:3] == "UTC"):
+                timeoffset = get_utc_offset(decoded_input)
+                realoffset = timeoffset * 3600
+                #sendserial(str(timeoffset))
+            else:
+                sendserial(str(input))
+                pass
+                #uart.write(str(input))
+        utime.sleep(7)
+        calltimer -= 1
